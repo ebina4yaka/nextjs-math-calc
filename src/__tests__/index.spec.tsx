46f85ac4e00d8a0d5936521a0b6e75d4a512dd35
ebina4yaka <4yaka3bina@gmail.com>
2020-07-08 09:08:11 +0900

    move index snapshot test

--- src/__tests__/index.spec.tsx
+++ src/__tests__/index.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import Index from '../index'
+import Index from '../pages'
 
 test('Index', () => {
   const tree = renderer.create(<Index />).toJSON()
