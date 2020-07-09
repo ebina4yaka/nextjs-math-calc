b69bfd33a19650b25c5aea18a14fe995a04f6b62
ebina4yaka <4yaka3bina@gmail.com>
2020-07-09 15:03:22 +0900

    add input onChange handle test

---
+++ src/molecules/__tests__/SummationInputField.spec.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { fireEvent, render } from '@testing-library/react'
+import { renderHook, act } from '@testing-library/react-hooks'
+import SummationInputField from '../SummationInputField'
+import UseSummation from '../../hooks/UseSummation'
+
+describe('SummationInputField Tests', () => {
+  test('Input onChange Handle Test', () => {
+    const { result } = renderHook(() => UseSummation())
+    const container = render(
+      <SummationInputField
+        n={result.current.n}
+        k={result.current.k}
+        a={result.current.a}
+        setNewSummation={result.current.setNewSummation}
+      />
+    )
+    const inputN = container.getByPlaceholderText('input-n') as HTMLInputElement
+    const inputK = container.getByPlaceholderText('input-k') as HTMLInputElement
+    const inputA = container.getByPlaceholderText('input-a') as HTMLInputElement
+    const newN = 20
+    const newK = 10
+    const newA = 5
+    act(() => {
+      fireEvent.change(inputN, { target: { value: newN } })
+    })
+    act(() => {
+      fireEvent.change(inputK, { target: { value: newK } })
+    })
+    act(() => {
+      fireEvent.change(inputA, { target: { value: newA } })
+    })
+    expect(result.current.n).toBe(newN)
+    expect(result.current.k).toBe(newK)
+    expect(result.current.a).toBe(newA)
+  })
+})
