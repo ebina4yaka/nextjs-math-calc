1eb7058b3b081f4885c677f568836b8d83dcaaa3
ebina4yaka <4yaka3bina@gmail.com>
2020-07-13 12:48:39 +0900

    fix tests

--- src/organisms/__tests__/Menu.spec.tsx
+++ src/organisms/__tests__/Menu.spec.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { act } from '@testing-library/react-hooks'
 import Menu from '../Menu'
 
 test('Menu Snapshot Test', () => {
-  const tree = renderer.create(<Menu />).toJSON()
-  expect(tree).toMatchSnapshot()
+  act(() => {
+    const tree = renderer.create(<Menu />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })
