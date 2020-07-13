2211cb472a99aad1c5ef9c292b28f23db4374ac1
ebina4yaka <4yaka3bina@gmail.com>
2020-07-13 11:19:23 +0900

    use json

--- src/organisms/ContentList.tsx
+++ src/organisms/ContentList.tsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ContentCard from '../molecules/ContentCard'
 
-export default function SummationComponent(): React.ReactElement {
-  return (
-    <ContentCard
-      url="/summation"
-      image="/static/image/summation.png"
-      title="Summation"
-    />
-  )
+type Json = {
+  data: Content[]
+  Response: string
+  Error: null
+}
+
+type Content = {
+  id: number
+  title: string
+  url: string
+  image: string
+}
+
+export default function ContentList(): React.ReactElement {
+  const [contents, setContents] = useState(Array<Content>())
+  const json = `${window.location.protocol}//${window.location.host}/data/contentList.json`
+
+  useEffect(() => {
+    fetch(json)
+      .then((response) => response.json())
+      .then((jsonResponse: Json) => {
+        setContents(jsonResponse.data)
+      })
+  }, [])
+
+  const createContents = (): JSX.Element[] => {
+    const list: JSX.Element[] = []
+    contents.map((content: Content) =>
+      list.push(
+        <ContentCard
+          key={content.id}
+          title={content.title}
+          url={content.url}
+          image={content.image}
+        />
+      )
+    )
+    return list
+  }
+
+  return <>{createContents()}</>
 }
